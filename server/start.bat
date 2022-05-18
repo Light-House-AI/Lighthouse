205f3b8a01e94b09b0527dbf1570b74240d32d7b
Ahmed Hussein <ahmedhussiien@gmail.com>
2022-05-18 13:46:35 +0200

    chore(server): add start scripts

---
+++ server/start.bat
@@ -0,0 +1,5 @@
+:: Run migrations
+:: alembic -c ml_projects/db/alembic.ini upgrade head
+
+:: Run lighthouse server
+python -m lighthouse
---
+++ server/start.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Run migrations
+# alembic -c ml_projects/db/alembic.ini upgrade head
+
+# Run lighthouse server
+python -m lighthouse
