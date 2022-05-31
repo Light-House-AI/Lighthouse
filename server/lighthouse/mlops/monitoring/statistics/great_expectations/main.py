31d0a02c2bc46ad4a81c0576d79b4fc142a5a35f
Ahmed Ayman <ahmed.ayman16k@gmail.com>
2022-05-31 21:05:20 +0200

    docs(monitoring)(ge): read me + update.gitignore

--- server/lighthouse/mlops/monitoring/statistics/great_expectations/main.py
+++ server/lighthouse/mlops/monitoring/statistics/great_expectations/main.py
@@ -1,13 +1,14 @@
 from services.helpers import *
 
 if __name__ == "__main__":
-    # *VARS
-    dataset_name = "yellow_tripdata_sample_2019-01.csv"
-    expectation_suite_name = "taxi-demo"
-    ##
+    # TODO get names from requester
+    # TODO separate to 2 functions
+    dataset_name = ""
+    expectation_suite_name = ""
+    dataset_name_2 = ""
+
     expectations_suite = generate_data_statistics(dataset_name,
                                                   expectation_suite_name)
-    # * VARS
-    dataset_name_2 = "yellow_tripdata_sample_2019-02.csv"
+
     html_content = validate_original_data_with_new(
         dataset_name_2, expectations_suite, expectation_suite_name)
