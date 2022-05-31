22e776664f74206215364160f964e942e198d08e
Ahmed Hussein <ahmedhussiien@gmail.com>
2022-05-31 10:15:24 +0200

    feat(monitoring): add /predict endpoint

--- server/lighthouse/ml_projects/api/routers/deployments.py
+++ server/lighthouse/ml_projects/api/routers/deployments.py
@@ -5,6 +5,7 @@ from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 
 from lighthouse.ml_projects.api import get_session, get_current_user_data
+from lighthouse.ml_projects.services import deployments as deployments_service
 from lighthouse.ml_projects.api.responses import (not_authenticated_response,
                                                   not_found_response)
 
@@ -22,6 +23,9 @@ def get_prediction(*,
                    db: Session = Depends(get_session),
                    user_data=Depends(get_current_user_data)):
     """
-    Returns current user projects.
+    Returns the predictions from the deployed model.
     """
-    print(deployment_id, input_data, user_data)
+    return deployments_service.get_prediction(deployment_id=str(deployment_id),
+                                              user_id=user_data.user_id,
+                                              input_data=input_data,
+                                              db=db)
