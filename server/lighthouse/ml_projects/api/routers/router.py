b78c311008f049b53433409079598a26554ffea0
Ahmed Hussein <ahmedhussiien@gmail.com>
2022-05-31 06:22:04 +0200

    feat(ml-projects): add health endpoint

--- server/lighthouse/ml_projects/api/routers/router.py
+++ server/lighthouse/ml_projects/api/routers/router.py
@@ -1,5 +1,13 @@
+from sys import prefix
 from fastapi.routing import APIRouter
-from . import monitoring
+from lighthouse.config import config
 
-api_router = APIRouter()
+from . import monitoring
+from . import projects
+from . import auth
+
+api_router = APIRouter(prefix=config.API_PREFIX)
+
 api_router.include_router(monitoring.router, tags=["monitoring"])
+api_router.include_router(projects.router, tags=["projects"])
+api_router.include_router(auth.router, tags=["auth"])
