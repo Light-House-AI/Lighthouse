72ed4125842b9ca6a35812c3dd144a10505bf6dc
Ahmed Hussein <ahmedhussiien@gmail.com>
2022-05-31 10:19:39 +0200

    fix(monitoring): fix mongo connect return type

--- server/lighthouse/mlops/monitoring/db/mongo.py
+++ server/lighthouse/mlops/monitoring/db/mongo.py
@@ -1,7 +1,7 @@
-from mongoengine import connect
+from mongoengine import connect, MongoClient
 from lighthouse.config import config
 
-def connect_to_mongo(db_host :str = config.MONGO_URI) -> None:
+def connect_to_mongo(db_host :str = config.MONGO_URI) -> MongoClient:
     """
     Connect to the specified database.
     """
