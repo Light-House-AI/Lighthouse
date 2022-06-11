534963828d176c98852cf4636e6c54e8ae0cdf79
Omar Elwakil <omartelwakil@gmail.com>
2022-06-11 04:51:57 +0200

    fix(client)!: fix filenames structure homepage/loginpage

---
+++ client/src/pages/HomePage.jsx
@@ -0,0 +1,37 @@
+import React, { useEffect } from "react";
+
+import Navigation from "../components/structure/Navigation";
+import Footer from "../components/structure/Footer";
+import Project from "../components/Project";
+
+function HomePage() {
+    useEffect(() => {
+        document.getElementsByTagName("body")[0].classList.add("overflow-y-scroll");
+    }, []);
+
+    return (
+        <div id="wrapper">
+            <title>Home | Lighthouse AI</title>
+            <Navigation />
+            <div className="homepage container-fluid mb-3">
+                <div className="row mx-2">
+                    <div className="col-xl-4 col-md-6">
+                        <Project data={{ name: 'Project 1', datasets: 3, models: 3, deployments: 3 }} _id={1} />
+                    </div>
+                    <div className="col-xl-4 col-md-6">
+                        <Project data={{ name: 'Project 2', datasets: 3, models: 3, deployments: 3 }} _id={2} />
+                    </div>
+                    <div className="col-xl-4 col-md-6">
+                        <Project data={{ name: 'Project 2', datasets: 3, models: 3, deployments: 3 }} _id={3} />
+                    </div>
+                    <div className="col-12">
+                        <Footer />
+                    </div>
+                </div>
+            </div>
+
+        </div >
+    );
+}
+
+export default HomePage;
