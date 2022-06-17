6d15acadb0164d4e23425f5080a345f05860dfd6
Omar Elwakil <omartelwakil@gmail.com>
2022-06-17 20:39:14 +0200

    feat(client): integrated view all deployments

--- client/src/components/Deployments.jsx
+++ client/src/components/Deployments.jsx
@@ -9,7 +9,8 @@ function Deployments(props) {
     const [rootCreated, setRootCreated] = useState(false);
     const [reactRoot, setReactRoot] = useState(null);
     const [deployments, setDeployments] = useState(null);
-    
+    const [models, setModels] = useState(null);
+
     useEffect(() => {
         window.tippy('[data-plugin="tippy"]', {
             placement: 'left',
@@ -17,8 +18,88 @@ function Deployments(props) {
             arrow: 'true'
         });
 
+        axios.get('/models', {
+            params: {
+                project_id: parseInt(projectId)
+            },
+            headers: {
+                'Authorization': localStorage.getItem('tokenType') + ' ' + localStorage.getItem('accessToken')
+            }
+        }).then((response) => {
+            setModels(response.data);
+        }).catch((error) => {
+        });
+
+        axios.get('/deployments', {
+            params: {
+                project_id: projectId
+            },
+            headers: {
+                'Authorization': localStorage.getItem('tokenType') + ' ' + localStorage.getItem('accessToken')
+            }
+        }).then((response) => {
+            console.log(response.data);
+            setDeployments(response.data);
+        }).catch((error) => {
+        });
+
         window.$("#testing").footable();
     }, []);
+
+    const createModel = () => {
+        if (deployments.length === 0 || deployments === null)
+            return;
+
+        var modalBody = reactRoot;
+        if (!rootCreated) {
+            modalBody = ReactDOM.createRoot(window.$("#modal-structure #modal-body")[0])
+            setRootCreated(true);
+            setReactRoot(modalBody);
+        }
+
+        let selectDS =
+            <div className="h-50 my-2">
+                <label className="form-label">Select Dataset:</label><br />
+                <select id="select-multiple-datasets">
+                    {deployments !== null ?
+                        deployments.map((dataset, index) => {
+                            return (
+                                <option key={index} value={dataset.id}>{dataset.name}</option>
+                            )
+                        })
+                        : null}
+                </select>
+            </div>;
+        modalBody.render(selectDS);
+
+        window.$("#modal-structure #modal-title").text("Create new cleaned dataset");
+        setTimeout(() => {
+            window.selectizeModal = window.$("#modal-structure #select-multiple-datasets").selectize({
+                maxItems: 1
+            });
+        }, 100);
+
+        window.$("#modal-structure #modal-btn").on('click', function () {
+            let datasetsId = window.selectizeModal[0].selectize.getValue();
+            if (datasetsId !== '' && datasetsId !== null && datasetsId !== [] && datasetsId !== undefined) {
+                if (Array.isArray(datasetsId))
+                    window.location.href = `/${projectId}/datasets/${datasetsId.join('-')}/clean`;
+                else
+                    window.location.href = `/${projectId}/datasets/${datasetsId}/clean`;
+            }
+        });
+
+        document.getElementById('modal-trigger').click();
+    }
+
+    const findModelName = function () {
+        for (let i = 0; i < models.length; i++) {
+            if (models[i].id === this.dataset_id) {
+                return models[i].name;
+            }
+        }
+    }
+
     return (
         <div className="row">
             <div className="col-12">
@@ -27,7 +108,7 @@ function Deployments(props) {
                         <h4 className="header-title mb-0 h-100 d-flex align-items-center">All Deployments</h4>
                     </div>
                     <div className="col-6">
-                        <button className="button-no-style float-end" title="Add new deployment" data-plugin="tippy">
+                        <button className="button-no-style float-end" title="Add new deployment" data-plugin="tippy" onClick={createModel}>
                             <i className="fe-plus noti-icon text-color btn-link"></i>
                         </button>
                     </div>
@@ -41,42 +122,35 @@ function Deployments(props) {
                                 <th>Type</th>
                                 <th>Status</th>
                                 <th>Monitoring</th>
-                                <th data-hide="all">Models</th>
+                                <th data-hide="all">Primary Model</th>
+                                <th data-hide="all">Secondary Model</th>
                             </tr>
                         </thead>
                         <tbody>
-                            <tr>
-                                <td>App design and development</td>
-                                <td>Jan 03, 2015</td>
-                                <td><span className="badge bg-soft-info text-info p-1">Champion - Challenger</span></td>
-                                <td><span className="badge bg-soft-success text-success p-1">Active</span></td>
-                                <td><button className="btn btn-outline-success btn-sm rounded-pill">View</button></td>
-                                <td>Model 1</td>
-                            </tr>
-                            <tr>
-                                <td>Coffee detail page - Main Page</td>
-                                <td>Sep 21, 2016</td>
-                                <td><span className="badge bg-soft-warning text-warning p-1">Fallout</span></td>
-                                <td><span className="badge bg-soft-success text-success p-1">Active</span></td>
-                                <td><button className="btn btn-outline-success btn-sm rounded-pill">View</button></td>
-                                <td>Model 2</td>
-                            </tr>
-                            <tr>
-                                <td>Poster illustation design</td>
-                                <td>Mar 08, 2018</td>
-                                <td><span className="badge bg-soft-info text-info p-1">Champion - Challenger</span></td>
-                                <td><span className="badge bg-soft-danger text-danger p-1">Disabled</span></td>
-                                <td><button className="btn btn-outline-success btn-sm rounded-pill disabled">View</button></td>
-                                <td>Model 1, Model 2</td>
-                            </tr>
-                            <tr>
-                                <td>Coffee detail page - Main Page</td>
-                                <td>Sep 21, 2016</td>
-                                <td><span className="badge bg-soft-warning text-warning p-1">Fallout</span></td>
-                                <td><span className="badge bg-soft-danger text-danger p-1">Disabled</span></td>
-                                <td><button className="btn btn-outline-success btn-sm rounded-pill disabled">View</button></td>
-                                <td>Model 2</td>
-                            </tr>
+                            {deployments !== null ?
+                                deployments.map((deployment, index) => {
+                                    return (
+                                        <tr key={index}>
+                                            <td>{deployment.name}</td>
+                                            <td>{(new Date(deployment.created_at).toLocaleString('en-CA', { month: 'short', day: 'numeric', year: 'numeric' }))}</td>
+                                            {deployment.type === 'champion_challenger' ?
+                                                <td><span className="badge bg-soft-info text-info p-1">Champion - Challenger</span></td> :
+                                                <td><span className="badge bg-soft-warning text-warning p-1">Fallout</span></td>
+                                            }
+                                            {deployment.is_running ?
+                                                <td><span className="badge bg-soft-success text-success p-1">Active</span></td> :
+                                                <td><span className="badge bg-soft-danger text-danger p-1">Disabled</span></td>
+                                            }
+                                            {deployment.is_running ?
+                                                <td><button className="btn btn-outline-success btn-sm rounded-pill">View</button></td> :
+                                                <td><button className="btn btn-outline-success btn-sm rounded-pill disabled">View</button></td>
+                                            }
+                                            <td>{findModelName(deployment.primary_model_id)}</td>
+                                            <td>{deployment.type === 'champion_challenger' ? deployment.secondary_model_id : 'No secondary model'}</td>
+                                        </tr>
+                                    )
+                                })
+                                : null}
                         </tbody>
                     </table>
                 </div>
