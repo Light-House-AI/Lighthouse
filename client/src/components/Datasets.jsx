086c28a7971a6f44f63a2a572580e3663c1dff91
Omar Elwakil <omartelwakil@gmail.com>
2022-06-17 13:50:45 +0200

    feat(client): select multiple datasets to clean

--- client/src/components/Datasets.jsx
+++ client/src/components/Datasets.jsx
@@ -1,11 +1,15 @@
 import React, { useState, useEffect } from "react";
+import ReactDOM from 'react-dom/client';
 import axios from "axios";
+
+import GenericModal from './structure/GenericModal';
 
 function Datasets(props) {
     const [projectId] = useState(props.projectId);
     const [rawDatasets, setRawDatasets] = useState(null);
     const [cleanedDatasets, setCleanedDatasets] = useState(null);
-
+    const [rootCreated, setRootCreated] = useState(false);
+    const [reactRoot, setReactRoot] = useState(null);
     useEffect(() => {
         window.tippy('[data-plugin="tippy"]', {
             placement: 'left',
@@ -38,6 +42,50 @@ function Datasets(props) {
         });
 
     }, []);
+
+    const createNewCleanedDS = () => {
+        var modalBody = reactRoot;
+        if (!rootCreated) {
+            modalBody = ReactDOM.createRoot(window.$("#modal-structure #modal-body")[0])
+            setRootCreated(true);
+            setReactRoot(modalBody);
+        }
+
+        let selectDS =
+            <div className="h-50 my-2">
+                <label className="form-label">Select Dataset(s):</label><br />
+                <select id="select-multiple-datasets">
+                    {rawDatasets !== null ?
+                        rawDatasets.map((dataset, index) => {
+                            return (
+                                <option key={index} value={dataset.id}>{dataset.name}</option>
+                            )
+                        })
+                        : null}
+                </select>
+            </div>;
+        modalBody.render(selectDS);
+
+        window.$("#modal-structure #modal-title").text("Create new cleaned dataset");
+        setTimeout(() => {
+            window.selectizeModal = window.$("#modal-structure #select-multiple-datasets").selectize({
+                maxItems: rawDatasets.length
+            });
+        }, 100);
+
+        window.$("#modal-structure #modal-btn").on('click', function () {
+            let datasetsId = window.selectizeModal[0].selectize.getValue();
+            if (datasetsId !== '' && datasetsId !== null && datasetsId !== [] && datasetsId !== undefined) {
+                if (Array.isArray(datasetsId))
+                    window.location.href = `/${projectId}/datasets/${datasetsId.join('-')}/clean`;
+                else
+                    window.location.href = `/${projectId}/datasets/${datasetsId}/clean`;
+            }
+        });
+
+        document.getElementById('modal-trigger').click();
+    }
+
     return (
         <div className="row">
             <div className="col-12">
@@ -82,7 +130,7 @@ function Datasets(props) {
                         <h4 className="header-title mb-0 h-100 d-flex align-items-center">Cleaned Datasets</h4>
                     </div>
                     <div className="col-6">
-                        <button className="button-no-style float-end" title="Clean Raw Dataset" data-plugin="tippy">
+                        <button className="button-no-style float-end" title="Clean Raw Dataset" data-plugin="tippy" onClick={createNewCleanedDS}>
                             <i className="fe-plus noti-icon text-color btn-link"></i>
                         </button>
                     </div>
@@ -115,6 +163,7 @@ function Datasets(props) {
                     </div>
                 </div>
             </div>
+            <GenericModal hideNext={false} />
         </div>
     );
 }
