8667e23914a85377e1a7fda4da18bf9b1b15cc96
Omar Elwakil <omartelwakil@gmail.com>
2022-06-21 11:31:16 +0200

    fix(visualization): return json not array

--- server/lighthouse/automl/data_cleaning/visualization.py
+++ server/lighthouse/automl/data_cleaning/visualization.py
@@ -9,10 +9,9 @@ def get_columns_visualization(df, columns):
     This function returns a dataframe with the columns selected by the user.
     """
     drop_missing_values(df, columns)
-    json_array = []
+    json_array = {}
     for col in columns:
         json = {}
-        json.update({"column_name": col})
 
         is_numeric, _ = is_numeric_or_categorical(df, col)
         json.update({"is_numeric": is_numeric})
@@ -20,9 +19,9 @@ def get_columns_visualization(df, columns):
         if is_numeric:
             json.update({'min': df[col].min(), 'max': df[col].max()})
 
-        json_array.append(json)
+        json_array.update({col: json})
         
-    json_array.append({'data': df[columns]})
+    json_array.update({'data': df[columns]})
     
     return json_array
 
