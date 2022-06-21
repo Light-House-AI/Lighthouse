7e23781d6a71fb5f1ff8023d425b21496c89d573
Omar Elwakil <omartelwakil@gmail.com>
2022-06-21 03:48:12 +0200

    fix(client): static components footer/navigation

--- client/src/components/structure/Footer.jsx
+++ client/src/components/structure/Footer.jsx
@@ -1,18 +1,21 @@
 import React, { useState } from 'react';
 
-function Footer() {
+function Footer(props) {
+    const [positionBottom] = useState(props.positionBottom === undefined || props.positionBottom === null ? false : props.positionBottom);
     const [year] = useState(new Date().getFullYear());
     return (
-        <div className="footer">
-            <div className="row">
-                <div className="col-md-6">
-                    {year} &copy; <a href="/">Lighthouse AI</a>
-                </div>
-                <div className="col-md-6">
-                    <div className="text-md-end footer-links d-none d-sm-block">
-                        <a href="/">About Us</a>
-                        <a href="/">Help</a>
-                        <a href="/">Contact Us</a>
+        <div className={positionBottom ? 'w-100 position-absolute bottom-0' : 'w-100'}>
+            <div className={positionBottom ? 'footer ms-0 me-3' : "footer"}>
+                <div className="row">
+                    <div className="col-md-6">
+                        {year} &copy; <a href="/">Lighthouse AI</a>
+                    </div>
+                    <div className="col-md-6">
+                        <div className="text-md-end footer-links d-none d-sm-block">
+                            <a href="/">About Us</a>
+                            <a href="/">Help</a>
+                            <a href="/">Contact Us</a>
+                        </div>
                     </div>
                 </div>
             </div>
--- client/src/components/structure/Navigation.jsx
+++ client/src/components/structure/Navigation.jsx
@@ -31,7 +31,6 @@ function Navigation() {
             });
         }, 5000);
 
-        clearInterval(window.notificationsIntervalId);
 
     }, []);
 
